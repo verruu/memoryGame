d9aa31506e89e3763d5d059e66ff3ba1f9849a5e
verruu <gacek333@gmail.com>
2022-08-03 16:20:22 +0200

    Second commit, building cards grid based on randomised words from file

--- src/Game.java
+++ src/Game.java
@@ -2,14 +2,24 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Game {
-    public static ArrayList<String> words;
+    public static ArrayList<String> words = new ArrayList<String>();
     public static String [][] board = new String[4][4];
     public static String [][] cards = new String[4][4];
     public static Scanner scanner = new Scanner(System.in);
+    private static int level;
+
+    public static int getLevel() {
+        return level;
+    }
+
+    public static void setLevel(int lvl) {
+        level = lvl;
+    }
 
     public static void printBoard() {
         for (int i = 0; i < 4; i++) {
@@ -22,31 +32,79 @@ public class Game {
         }
     }
 
+    public static void printCards() {
+        for (int i = 0; i < 4; i++) {
+            System.out.print("|");
+            for (int j = 0; j < 4; j++) {
+                System.out.print(cards[i][j]);
+                System.out.print("|");
+            }
+            System.out.println();
+        }
+    }
+//    SHUFFLE CARDS BASED ON CHOSEN GAME DIFFICULTY. RESETTING ARRAYLISTS.
     public static void shuffleCards() { //row? col?
         Random rand = new Random();
-        ArrayList<String> cardsList = new ArrayList<String>();
-//        Add cards from randomizer to cardsList
         int index;
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                index = rand.nextInt(cardsList.size());
-                cards[i][j] = cardsList.get(index);
-                cardsList.remove(index);
+        words.addAll(words);
+        if (level == 1) {
+            for (int i = 0; i < 2; i++) {
+                for (int j = 0; j < 4; j++) {
+                    index = rand.nextInt(words.size());
+                    cards[i][j] = words.get(index);
+                    words.remove(index);
+                }
+            }
+        } else if (level == 2) {
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 4; j++) {
+                    index = rand.nextInt(words.size());
+                    cards[i][j] = words.get(index);
+                    words.remove(index);
+                }
             }
         }
     }
-
-    public static void fileReader() {
+//    BUILD A BASE OF WORDS BASED ON CHOSEN GAME DIFFICULTY. USES ARRAYLIST CREATED BY FILEREADER METHOD
+//    RETURNS ARRAYLIST OF TYPE STRING (SINGLE ITERATION OF RANDOMIZED WORD)
+    public static ArrayList<String> wordsPoolBuilder() {
+        Random rand = new Random();
+        ArrayList<String> tempList = fileReader();
+        int index;
+        if (level == 1) { // EASY: NEED 4 WORDS
+            for (int i = 0; i < 4; i++) {
+                index = rand.nextInt(tempList.size());
+                words.add(tempList.get(index));
+                tempList.remove(index);
+            }
+        } else if (level == 2) { // HARD: NEED 8 WORDS
+            for (int i = 0; i < 8; i++) {
+                index = rand.nextInt(tempList.size());
+                words.add(tempList.get(index));
+                tempList.remove(index);
+            }
+        }
+        return words;
+    }
+//    READ TEXT FILE LINE BY LINE AND RETURN CONTENT IN AN ARRAYLIST OF TYPE STRING (WORDS)
+    public static ArrayList<String> fileReader() {
+        ArrayList<String> tempList = new ArrayList<String>();
         try {
             BufferedReader reader = new BufferedReader(new FileReader("Words.txt"));
             String line;
             while((line = reader.readLine()) != null) {
-                System.out.println(line);
-                words.add(line);
+                tempList.add(line);
             }
             reader.close();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+        return tempList;
+    }
+
+    public static void simplePrinter(List<String> list) {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
         }
     }
 
@@ -110,26 +168,38 @@ public class Game {
     }
 
     public static void main(String[] args) {
+
+//        simplePrinter(fileReader());
         while (true) {
-            System.out.println("Press n for a new game or q to quit.");
-            String choice = scanner.nextLine();
-            if (choice.equals("q")) {
-                System.out.println("Exitting the game.");
-                break;
-            } else if (choice.equals("n")) {
-                shuffleCards();
-                for (int i = 0; i < 4; i++) {
-                    for (int j = 0; j < 4; j++) {
-                        board[i][j] = " _ ";
-                    }
-                }
-                printBoard();
-                checkInput(cards);
-                break;
-            } else {
-                System.out.println("Invalid input.");
-                continue;
-            }
+            setLevel(2);
+            simplePrinter(wordsPoolBuilder());
+            shuffleCards();
+            printBoard();
+            printCards();
+            break;
         }
+
+
+//        while (true) {
+//            System.out.println("Press n for a new game or q to quit.");
+//            String choice = scanner.nextLine();
+//            if (choice.equals("q")) {
+//                System.out.println("Exitting the game.");
+//                break;
+//            } else if (choice.equals("n")) {
+//                shuffleCards();
+//                for (int i = 0; i < 4; i++) {
+//                    for (int j = 0; j < 4; j++) {
+//                        board[i][j] = " _ ";
+//                    }
+//                }
+//                printBoard();
+//                checkInput(cards);
+//                break;
+//            } else {
+//                System.out.println("Invalid input.");
+//                continue;
+//            }
+//        }
     }
 }
